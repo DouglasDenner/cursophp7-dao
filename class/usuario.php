5dff9c4763966efb75dadff47d4ff5085ed48293
Douglas Denner <doh_guedes@hotmail.com>
2025-09-17 09:04:31 -0300

    metodos lista

--- class/usuario.php
+++ class/usuario.php
@@ -49,8 +49,6 @@ public function loadById($id){
         $this->setDtcadastro(new DateTime($row['dtcadastro']));
 
     }
-
-
 }
 public function __toString(){
 
@@ -62,6 +60,50 @@ public function __toString(){
     ));
 }   
 
+   public static function getList(){
+
+        $sql = new sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+
 }
 
+public static function search($login){
+
+    $sql = new sql();
+
+    return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+        ':SEARCH'=>"%".$login."%"
+    ));
+
+
+}
+
+public function login($login, $password){
+
+    $sql = new sql();
+
+    $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+        ":LOGIN"=>$login,
+        ":PASSWORD"=>$password
+    ));
+
+    if (count($results) > 0){
+
+        $row = $results[0];
+        $this->setIdusuario($row['idusuario']);
+        $this->setDeslogin($row['deslogin']);
+        $this->setDessenha($row['dessenha']);
+        $this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+    } else {
+
+        throw new Exception("Login e/ou senha inválidos.");
+
+    }
+
+
+}
+}
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
 require_once("config.php");
 require_once("sql.php"); 
-$root = new Usuario();
-$root->loadById(3);
+//$root = new Usuario();
+//$root->loadById(3);
 
-echo $root;
+//echo $root;
 
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//$search = Usuario::search("ca");
+
+//echo json_encode($search);
+
+$usuario = new Usuario();
+$usuario->login("douglas", "12345");
+echo $usuario;
 
 
 
